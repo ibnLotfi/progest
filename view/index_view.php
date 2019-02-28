9cfbb61277c93dce360951f0b3fa4ce135807a8b
Issam Ben Mabrouk <ibnlotfi@hotmail.fr>
2019-02-28 14:44:30 +0100

    Cree la structure du projet

---
+++ view/index_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="static/auction.css"/>
+        <title>Projet collaboratif</title>
+    </head>
+    <body>
+        <?php
+        // Header shared by all pages
+        require_once("header.php");
+        ?>
+        <h1>Projet collaboratif</h1>
+        <p>Ici la page d'accueil</p>
