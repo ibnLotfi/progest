482d22792f829b04ddda247c6abddfd7135a0827
kumanan <>
2019-03-01 14:42:06 +0100

    refactor personne_controlle en etudiant_controller ajout de professeur controller modif index php

--- pages/index.php
+++ pages/index.php
@@ -1,14 +1,18 @@
 <?php
+
 session_start();
 require_once("../view/index_view.php");
 
 if (isset($_SESSION["user"])) {
-           // $nom = $_SESSION["user"] ;
-            //echo "Bienvenue ".$_SESSION["user"]["prenom"];
-           // if($_SESSION["user"])
-            require_once("../pages/etudiant_controller.php");
-           
-            }
-            else{
-                 echo "vous etes deconnecter";
-            }
+    // $nom = $_SESSION["user"] ;
+    //echo "Bienvenue ".$_SESSION["user"]["prenom"];
+    if ($_SESSION["user"]["ETREPROF"] == 0) {
+        require_once("../pages/etudiant_controller.php");
+    } 
+    else {
+        require_once("../pages/professeur_controller.php");
+    }
+}
+else {
+    echo "Vous etes deconnecter";
+}
---
+++ pages/professeur_controller.php
@@ -0,0 +1,8 @@
+<?php
+echo "vous etes sur la page controller professeur";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
