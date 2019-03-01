27f88420e27e79fbaf93c811e47ea6cdf238da48
Frederic Vermont <f.vermont@wanadoo.fr>
2019-03-01 14:52:39 +0100

    Ajout de getProjet dans index

--- pages/etudiant_controller.php
+++ pages/etudiant_controller.php
@@ -2,8 +2,8 @@
 echo '<br> <br> ceci est le controller etudiant';
 
 // A cree 
-$promotion = Promotion::getPromotion($_SESSION["user"]["id"]);
-
+$promotion = Promotion::get($_SESSION["user"]["id"]);
+$lesProjets=Projet::get($_SESSION["user"]["id"]);
 include "../view/etudiant_view.php";
 /* 
  * To change this license header, choose License Headers in Project Properties.
