eeb07fc1e855a5296a114c379d7f7a0171017c09
Chendet <48014250+Chendet@users.noreply.github.com>
2019-03-01 16:29:03 +0100

    modification de progest_reset.sql
    modification du fichier de la procedure stocké stable et fonctionnant

--- resources/progest_reset.sql
+++ resources/progest_reset.sql
@@ -1,6 +1,8 @@
 -- procedure stocké progest_reset
 
-CREATE OR REPLACE PROCEDURE progest_reset(v_datetime datetime)
+--call progest_reset('2019-03-01');
+
+CREATE OR REPLACE PROCEDURE progest_reset(v_datetime date)
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -19,7 +21,7 @@ BEGIN
 	
 	-- Enable again foreign key constraints
 	--SET FOREIGN_KEY_CHECKS=1;
-	START TRANSACTION;
+	--START TRANSACTION;
 	INSERT INTO public.personne (id_personne, nom, prenom, adresse, ville, cp, tel, portable, email, mdp, etreprof) 
 	VALUES 
 	(1, 'Dupont', 'Julien', ' 2 rue Paul Vaillant Couturier', 'Cachan', '94230', '01874512', '06874512', 'jdupont@gmail.com', 'false', NULL),
@@ -39,10 +41,6 @@ BEGIN
 	(15, 'bmk', 'issam', ' 30 rue des chevaux', 'Paris', '75008', '0125478956', '0713762518', 'bissam@yahoo.fr', 'false', NULL);
 
 	
-	
-	
-	
-
 	INSERT INTO public.promotion (id_promotion,annee) 
 	VALUES 
 	(1,v_datetime),
@@ -51,12 +49,12 @@ BEGIN
 	
 	
 	
-	INSERT INTO public.projet (id_projet, titre, date_Creation_Projet, dateLimite, sujet) 
+	INSERT INTO public.projet (id_projet, titre, date_Creation_Projet, dateLimite, sujet, id_promotion) 
 	VALUES 
-	(1, "PROJET 1", v_datetime, v_datetime+interval '5 months',"php"),
-	(2, "PROJET 2", v_datetime-interval '3 months', v_datetime+interval '3 months',"java");
-	(3, "PROJET 3", v_datetime+interval '2 months', v_datetime+interval '5 months',"C#");
-	(4, "PROJET 4", v_datetime-interval '10 months', v_datetime-interval '2 months', "python");
+	(1, 'PROJET 1', v_datetime, v_datetime+interval '5 months','php',1),
+	(2, 'PROJET 2', v_datetime-interval '3 months', v_datetime+interval '3 months','java',1),
+	(3, 'PROJET 3', v_datetime+interval '2 months', v_datetime+interval '5 months','C#',1),
+	(4, 'PROJET 4', v_datetime-interval '10 months', v_datetime-interval '2 months', 'python',2);
 
 	
 	
@@ -67,19 +65,19 @@ BEGIN
 	
 	
 	--equipe 1 (id_equipe=1) du projet 1
-	INSERT INTO public.equipe (id_equipe,date_Creation_Equipe,commentaire) VALUES (1,v_datetime,"équipes alpha Projet 1");	
+	INSERT INTO public.equipe (id_equipe,id_projet,date_Creation_Equipe,commentaire,id_proprietaire) VALUES (1,1,v_datetime,'équipes alpha Projet 1',1);	
 	--equipe 2 (id_equipe=2) du projet 1
-	INSERT INTO public.equipe (id_equipe,date_Creation_Equipe,commentaire) VALUES (2,v_datetime,"équipes beta Projet 1");
+	INSERT INTO public.equipe (id_equipe,id_projet,date_Creation_Equipe,commentaire,id_proprietaire) VALUES (2,1,v_datetime,'équipes beta Projet 1',4);
 	
 	
 	--equipe 1 (id_equipe=3) du projet 2
-	INSERT INTO public.equipe (id_equipe,date_Creation_Equipe,commentaire) VALUES (3,v_datetime-interval '2 months',"equipe partiellement attribué");
+	INSERT INTO public.equipe (id_equipe,id_projet,date_Creation_Equipe,commentaire,id_proprietaire) VALUES (3,2,v_datetime-interval '2 months','equipe partiellement attribué',7);
 	
 	
 	--equipe 1 (id_equipe=3) du projet 4
-	INSERT INTO public.equipe (id_equipe,date_Creation_Equipe,commentaire) VALUES (4,v_datetime-interval '2 months',"equipes completement attribué et fini ");
+	INSERT INTO public.equipe (id_equipe,id_projet,date_Creation_Equipe,commentaire,id_proprietaire) VALUES (4,4,v_datetime-interval '2 months','equipes completement attribué et fini',1);
 	--equipe 2 (id_equipe=3) du projet 4
-	INSERT INTO public.equipe (id_equipe,date_Creation_Equipe,commentaire) VALUES (5,v_datetime-interval '10 months',"equipes completement attribué et fini ");
+	INSERT INTO public.equipe (id_equipe,id_projet,date_Creation_Equipe,commentaire,id_proprietaire) VALUES (5,4,v_datetime-interval '10 months','equipes completement attribué et fini',4);
 	
 	
 	
@@ -153,83 +151,3 @@ BEGIN
 	
 END;
 $$;
-
-
-
-
-
-
---v_datetime DATETIME
-
-
-
---exemple de procedure mySQL --
-/*CREATE PROCEDURE auction_reset(v_datetime DATETIME)
-BEGIN
-	IF v_datetime IS NULL THEN
-		SET v_datetime = NOW();
-	END IF;
-	-- Disable temporarily foreign key constraints
-	SET FOREIGN_KEY_CHECKS=0;
-	TRUNCATE TABLE bid;
-	TRUNCATE TABLE product;
-	TRUNCATE TABLE member;
-	TRUNCATE TABLE category;
-	-- Enable again foreign key constraints
-	SET FOREIGN_KEY_CHECKS=1;
-
-	START TRANSACTION;
-	INSERT INTO category (category_id, label) VALUES
-	(1, 'Comics'),
-	(2, 'CD');
-
-	INSERT INTO member (member_id, email, password, name, first_name, address, zip, city) VALUES
-	(1, 'haddock@moulinsart.be', 'capitaine', 'Haddock', 'Archibald', 'Chateau de Moulinsart', '01234', 'Moulinsart'),
-	(2, 'bianca.castafiore@scala.it', 'ah je ris', 'Castafiore', 'Bianca', 'Théâtre de la Scala', '09876', 'Milan'),
-	(3, 'tournesol@moulinsart.be', 'un peu plus à l''ouest', 'Tournesol', 'Tryphon', 'Château de Moulinsart', '01234', 'Moulinsart'),
-	(4, 'lampion@mondass.fr', 'Signez là', 'Lampion', 'Séraphin', '34 rue des jobards', '01234', 'Moulinsart'),
-	(5, 'nestor@moulinsart.be', 'Loch Lomond', '?', 'Nestor', 'Château de Moulinsart', '01234', 'Moulinsart');
-
-
-	INSERT INTO product (product_id, description, floor_price, deadline, auction_price, entry_date, 
-	seller_id, category_id) VALUES
-	(1, 'Le temple du soleil', 10.0, v_datetime + INTERVAL 1 DAY, 8.0, v_datetime - INTERVAL 10 DAY, 1, 1),
-	(2, 'Les bijoux de la Castafiore', 12.0, v_datetime + INTERVAL 1 DAY, 10.0, v_datetime - INTERVAL 6 DAY, 2, 1),
-	(3, 'Coke en stock', 11.0, v_datetime, 10.0, v_datetime - INTERVAL 5 DAY, 3, 1),
-	(4, 'Les picaros', 11.5, v_datetime - INTERVAL 1 DAY, 10.0, v_datetime - INTERVAL 5 DAY, 3, 1),
-	(5, 'Le tournoi des 3 licornes', 18.0, v_datetime - INTERVAL 1 DAY, 15.0, v_datetime - INTERVAL 8 DAY, 1, 1),
-	(6, 'Mafalda', 12.0, v_datetime, 11.0, v_datetime - INTERVAL 5 DAY, 3, 1),
-	(7, 'Köln concert', 12.0, v_datetime, 11.0, v_datetime - INTERVAL 4 DAY, 1, 2);
-
-	INSERT INTO bid (bid_id, amount, effect_time, bidder_id, product_id) VALUES
-	(1, 11.0, v_datetime - INTERVAL 8 DAY, 2, 1),
-	(2, 12.0, v_datetime - INTERVAL 7 DAY, 3, 1),
-	(3, 13.0, v_datetime - INTERVAL 7 DAY + INTERVAL 1 HOUR, 2, 1),
-	(4, 14.0, v_datetime - INTERVAL 6 DAY, 4, 1),
-	(5, 15.0, v_datetime - INTERVAL 4 DAY, 2, 1),
-	(6, 16.0, v_datetime - INTERVAL 2 DAY, 3, 1),
-
-	(7, 21.0, v_datetime - INTERVAL 5 DAY, 3, 2),
-	(8, 22.0, v_datetime - INTERVAL 4 DAY, 1, 2),
-	(9, 22.5, v_datetime - INTERVAL 3 DAY, 3, 2),
-	(10, 25.0, v_datetime - INTERVAL 2 DAY, 1, 2),
-
-	(11, 12.0, v_datetime - INTERVAL 4 DAY, 1, 3),
-	(12, 12.1, v_datetime - INTERVAL 3 DAY, 2, 3),
-	(13, 12.6, v_datetime - INTERVAL 2 DAY, 1, 3),
-	(14, 13.0, v_datetime - INTERVAL 2 DAY + INTERVAL 2 HOUR, 2, 3),
-
-	(15, 10.5, v_datetime - INTERVAL 4 DAY, 1, 4),
-	(16, 11.0, v_datetime - INTERVAL 3 DAY, 2, 4),
-	(17, 11.5, v_datetime - INTERVAL 2 DAY, 4, 4),
-	(18, 12.5, v_datetime - INTERVAL 2 DAY + INTERVAL 1 HOUR, 1, 4),
-	(19, 13.0, v_datetime - INTERVAL 1 DAY, 2, 4),
-
-	(20, 15.5, v_datetime - INTERVAL 7 DAY, 3, 5),
-	(21, 16.5, v_datetime - INTERVAL 6 DAY, 2, 5),
-	(22, 17.0, v_datetime - INTERVAL 5 DAY, 4, 5);
-
-	COMMIT;
-END$$
-
-*/
