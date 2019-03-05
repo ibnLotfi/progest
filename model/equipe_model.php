fabc44170632a79a29ed051a248e090893bccbca
CunhaCarlos <c.cunha@laposte.net>
2019-03-05 14:56:51 +0100

    Ajout de equipe_model.php avec fonction getEquipe.

---
+++ model/equipe_model.php
@@ -0,0 +1,27 @@
+<?php
+require_once("DB.php");
+
+/** Access to the equipe table.
+ * Put here the methods like getBySomeCriteriaSEarch */
+class Equipe {
+
+   /** Get equipe data for id $idpersonne
+    * @param int $idpersonne 
+    * @return associative_array table row
+    */
+   public static function getEquipe($idpersonne) {
+      $db = DB::getConnection();
+      $sql = "SELECT *
+              FROM equipe
+              WHERE id_equipe in (select id_equipe from membre_equipe where id_personne = :idpersonne )";
+      $stmt = $db->prepare($sql);
+      
+      $stmt->bindValue(":idpersonne", $idpersonne);
+      $ok = $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+   }
+
+   
+}
+
+?>
