3b317f1321f5bfc9a6c227e273ca1a4f4b1719ff
CunhaCarlos <c.cunha@laposte.net>
2019-03-07 11:45:21 +0100

    Modification dans projet_model.php avec fonction setProjet.

--- model/projet_model.php
+++ model/projet_model.php
@@ -38,13 +38,13 @@ class Projet {
       $ok = $stmt->execute();
       return $stmt->fetch(PDO::FETCH_ASSOC);
    }
-   public static function setProjet($idprojet,$titre,$date_limite,$sujet) {
+   public static function setProjet($titre,$date_limite,$sujet,$idpropriataire) {
       $db = DB::getConnection();
       
-      $sql = "insert into projet (id_projet,titre,date_limite,sujet)
-              values(:idprojet,:titre,:date_limite,:sujet))";
+      $sql = "insert into projet (id_proprietaire,titre,date_limite,sujet)
+              values(:idproprietaire,:titre,:date_limite,:sujet))";
       $stmt = $db->prepare($sql);
-      $stmt->bindValue(":idprojet", $idprojet);
+      $stmt->bindValue(":idprojet", $idproprietaire);
       $stmt->bindValue(":titre", $titre);
       $stmt->bindValue(":date_limite", $date_limite);
       $stmt->bindValue(":sujet", $sujet);
