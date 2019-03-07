258f67d883f5b4dafe80f1bbe1ef1b488e137e36
Kumanan <nesarajahkumanan@gmail.com>
2019-03-07 10:42:21 +0100

    modif equipeview

--- view/equipe_view.php
+++ view/equipe_view.php
@@ -1,13 +1,11 @@
-
+<?php $PromotionConcernee=' CDI8'?>
 <form action="etudiant_controller.php" method="post">
     <table>
         
          <p>Titre :<input type="text" name="titre" />  </p>
          <p>Propriétaire :<input type="text" name="propriétaire" /> </p>
-         <p>Date de création : <input type="text" name="dateDeCreation" /> </p>
-         <p>Date limite de rendu : <input type="text" name="dateLimiteDeRendu" /> </p>
-         <p>Sujet : <input type="text" name="sujet"  /> </p>
-         <p>Session de formation concernée : <input type="text" name="sessionDeFormation"  /> </p>
+         <p>Commentaire optionnel : <input type="text" name="CommentaireOptionnel" /> </p>
+         <p>Session de formation concernée :<?php echo $PromotionConcernee ?> </p>
          <p><input type="submit" value="OK"></p>
 
     </table>
