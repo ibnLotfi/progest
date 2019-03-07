dc7b4a6bb1b88c8547ff99818d2a2a7101d6e0c3
CunhaCarlos <c.cunha@laposte.net>
2019-03-07 14:07:07 +0100

    MAJ dans promotion_model.php sur fonction getPromotion.

--- model/promotion_model.php
+++ model/promotion_model.php
@@ -12,13 +12,13 @@ class Promotion {
      */
     public static function getPromotion($idpersonne) {
         $db = DB::getConnection();
-        $sql = "SELECT *
-              FROM membre_promotion
-              WHERE id_personne = :idpersonne ";
+        $sql = "SELECT annee
+              FROM promotion
+              WHERE id_promotion = (select id_promotion FROM membre_promotion WHERE id_personne = :idpersonne) ";
         $stmt = $db->prepare($sql);
         $stmt->bindValue(":idpersonne", $idpersonne);
         $ok = $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
