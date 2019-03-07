506aef7b4acbf0cd756979b016cac32c1954a338
CunhaCarlos <c.cunha@laposte.net>
2019-03-07 14:57:29 +0100

    Correction dans promotion_model.php et projet_model.php.

--- model/projet_model.php
+++ model/projet_model.php
@@ -38,20 +38,21 @@ class Projet {
       $ok = $stmt->execute();
       return $stmt->fetch(PDO::FETCH_ASSOC);
    }
-   public static function setProjet($titre,$date_limite,$sujet,$idpropriataire) {
+   public static function setProjet($titre,$date_limite,$sujet,$idproprietaire, $idpromotion) {
       $db = DB::getConnection();
       
-      $sql = "insert into projet (id_proprietaire,titre,date_limite,sujet)
-              values(:idproprietaire,:titre,:date_limite,:sujet))";
+      $sql = "insert into projet (id_proprietaire,titre,date_limite,sujet,id_promotion)
+              values(:idproprietaire,:titre,:date_limite,:sujet)";
       $stmt = $db->prepare($sql);
       $stmt->bindValue(":idproprietaire", $idproprietaire);
       $stmt->bindValue(":titre", $titre);
       $stmt->bindValue(":date_limite", $date_limite);
       $stmt->bindValue(":sujet", $sujet);
+      $stmt->bindValue(":idpromotion", $idpromotion);
       $ok = $stmt->execute();
       return $ok;
    }
-  public static function upProjet($idprojet,$titre,$date_limite,$sujet) {
+  public static function updateProjet($idprojet,$titre,$date_limite,$sujet) {
       $db = DB::getConnection();
       
       $sql = "UPDATE projet SET titre = :titre, date_limite = :date_limite, sujet = :sujet 
