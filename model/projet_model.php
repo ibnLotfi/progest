3c6287f23adf8d4002aa22c33f52eb9dd5ec273a
CunhaCarlos <c.cunha@laposte.net>
2019-03-07 11:46:13 +0100

    Modification dans projet_model.php avec fonction setProjet.

--- model/projet_model.php
+++ model/projet_model.php
@@ -44,7 +44,7 @@ class Projet {
       $sql = "insert into projet (id_proprietaire,titre,date_limite,sujet)
               values(:idproprietaire,:titre,:date_limite,:sujet))";
       $stmt = $db->prepare($sql);
-      $stmt->bindValue(":idprojet", $idproprietaire);
+      $stmt->bindValue(":idproprietaire", $idproprietaire);
       $stmt->bindValue(":titre", $titre);
       $stmt->bindValue(":date_limite", $date_limite);
       $stmt->bindValue(":sujet", $sujet);
